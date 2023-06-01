6c228e305dd851cb4b657e8ecf3432ff648eefd2
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-06-01 18:27:41 -0400

    Define spacing

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'preact/hooks'
 import './app.css'
 import mammoth from 'mammoth'
-import pretty from 'pretty' // add this import
+import pretty from 'pretty'
 
 export function App() {
   const [htmlContent, setHtmlContent] = useState('')
@@ -35,19 +35,25 @@ export function App() {
   return (
     <>
       <h1>Docx to HTML</h1>
+      <p>Use this tool to convert any DOCX file to an HTML to use in your blog. No data is collected. </p>
       <div class="card">
-        <input type="file" onChange={handleFileChange} />
+        <div className="file-upload-button" onChange={handleFileChange}>
+          <input type="file" id="file" style={{ display: 'none' }} />
+          <label for="file">Upload File</label>
+        </div>
         {htmlContent && (
           <>
-            <button onClick={() => setShowPretty(!showPretty)}>
+            <button className="toggle-button" onClick={() => setShowPretty(!showPretty)}>
               Toggle Compressed HTML
             </button>
-            <textarea
-              value={showPretty ? prettyHtml : htmlContent}
-              readOnly
-              rows={10}
-              style={{ width: '100%' }}
-            />
+            <div className="textarea-container">
+              <textarea
+                value={showPretty ? prettyHtml : htmlContent}
+                readOnly
+                rows={10}
+                style={{ width: '100%' }}
+              />
+            </div>
           </>
         )}
       </div>
